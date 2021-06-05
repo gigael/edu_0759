99dd185319ea6e18edfc4559d533ba0a927fa202
g_i_g@list.ru <gigael>
2021-06-05 11:32:19 +0300

    Исправление задачи 1_7

--- task_1.7/Main.java
+++ task_1.7/Main.java
@@ -13,11 +13,17 @@
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        int a = scan.nextInt();
-        int b = scan.nextInt();
-        int c = scan.nextInt();
-        int d = a * b * c;
-        System.out.println("нужно " + d + "000 литров воды");
+        System.out.println("Введите ширину бассейна в метрах");
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+
+        System.out.println("Введите длину бассейна в метрах");
+        int b = scanner.nextInt();
+
+        System.out.println("Введите высоту бассейна в метрах");
+        int c = scanner.nextInt();
+
+        System.out.println("Необходимо для заполнения бассейна литров:");
+        System.out.println(a*b*c*1000);
     }
 }
