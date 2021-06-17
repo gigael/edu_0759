b2e667895965716fc3aea63127f36c928616d424
Георгий Лисицын <85191266+gigael@users.noreply.github.com>
2021-06-17 11:54:07 +0300

    Update MultiThread.java

--- proxyChecker/MultiThread.java
+++ proxyChecker/MultiThread.java
@@ -1,1 +1,66 @@
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.InetSocketAddress;
+import java.net.Proxy;
+import java.net.URL;
+import java.net.URLConnection;
 
+public class Main {
+    public static void main(String[] args) {
+        try {
+            FileInputStream fis = new FileInputStream("C://java/ip.txt");
+            int i;
+            String proxy = "";
+            while((i=fis.read()) != -1){
+                if(i==13) continue;
+                else if(i==10){
+                    String ip = proxy.split(":")[0];
+                    String port = proxy.split(":")[1];
+                    CheckProxy checkProxy = new CheckProxy(ip, Integer.parseInt(port));
+                    checkProxy.start();
+                    proxy = "";
+                }else if(i!=9){
+                    proxy += (char) i;
+                }else{
+                    proxy += ":";
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+
+class CheckProxy extends Thread{
+    String ip;
+    int port;
+
+    public CheckProxy(String ip, int port) {
+        super();
+        this.ip = ip;
+        this.port = port;
+    }
+
+    @Override
+    public void run(){
+        try {
+            Proxy proxy = new Proxy(Proxy.Type.HTTP,new InetSocketAddress(ip,port));
+            URL url = new URL("https://vozhzhaev.ru/test.php");
+            URLConnection urlConnection = url.openConnection(proxy);
+            InputStream is = urlConnection.getInputStream();
+            InputStreamReader reader = new InputStreamReader(is);
+            int i;
+            StringBuilder result = new StringBuilder();
+            while ((i=reader.read()) != -1){
+                result.append((char)i);
+            }
+            System.out.println(result);
+        } catch (IOException exception) {
+            System.out.println(ip+" - не работает!");
+        }
+    }
+
+}
